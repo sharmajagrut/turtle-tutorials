3d7d993c5888f736a1dd7df830cf419ead2af15a
Jagrut Sharma <sharmajagrutfb@gmail.com>
2020-04-05 21:14:56 +0530

    added functions in swastik and square

--- turtle_swastik.py
+++ turtle_swastik.py
@@ -1,46 +1,46 @@
 import turtle
 
 t = turtle.Turtle()
-t.goto(0, 0)
-t.forward(100)
-t.right(90)
-t.forward(100)
+
+def make_L():
+
+    t.pendown()     # To make L
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.penup()
+    t.goto(0, 0)
+
+def turn():
+
+    t.right(180)    # To make turn
+
+def m_dot():
+
+    global a    # to make dots
+    global b
+    t.goto(a, b)
+    t.dot(10)
+
+
+make_L()
+turn()
+make_L()
+turn()
+make_L()
+turn()
+make_L()
+a = 50      # defining positions
+b = 50
+m_dot()
+b -= 100    # defining positions
+m_dot()
+a -= 100    # defining positions
+m_dot()
+b += 100    # defining positions
+m_dot()
 t.penup()
 t.goto(0, 0)
-t.pendown()
-t.left(180)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.penup()
-t.goto(0, 0)
-t.pendown()
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.penup()
-t.goto(0, 0)
-t.pendown()
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.penup()
-t.goto(50, 50)
-t.pendown()
-t.dot(10)
-t.penup()
-t.goto(50, -50)
-t.pendown()
-t.dot(10)
-t.penup()
-t.goto(-50, -50)
-t.pendown()
-t.dot(10)
-t.penup()
-t.goto(-50, 50)
-t.pendown()
-t.dot(10)
-t.penup()
-t.goto(0, 0)
+
 turtle.exitonclick()
+
--- tutrtle_square.py
+++ tutrtle_square.py
@@ -1,11 +1,17 @@
 import turtle
 
 t = turtle.Turtle()
-t.forward(200)
-t.right(90)
-t.forward(200)
-t.right(90)
-t.forward(200)
-t.right(90)
-t.forward(200)
+
+def square():
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)  # Completed a Square
+
+square()
+t.forward(100)
+square()
 turtle.exitonclick()
